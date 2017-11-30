24d95b0424b0ac1816527a73f343f2ac771b82e3
taufik2909 <taufikhidayat290999@gmail.com>
2017-12-01 00:38:47 +0700

    Update fftauto.js

--- fftauto.js
+++ fftauto.js
@@ -12,11 +12,11 @@ const rp = require('request-promise');
 const _ = require('lodash');
 
 const User = {
-    username: '',
-    password: ''
+    username: 'mhdtt_',
+    password: 'taufik4rt'
 }
 
-const TargetUsername = '';
+const TargetUsername = 'maharapall';
 
 const Login = async function(User){
 
